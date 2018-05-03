4f68bb8214d3b831b4ff88d75aa4866e45f4240f
Sameer Prabhune <sprabhune@VDRIVIT.COM>
2018-05-03 19:14:37 +0530

    Updated repos name

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker pull sprabhune/HelloNode:master
+docker pull sprabhune/Hellonode:master
 docker stack deploy --compose-file docker-compose.yml HelloNodeWebStack
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-docker rmi -f sprabhune/HelloNode:master || echo "master branch moving on"
-docker tag "sprabhune/HelloNode:${BUILD_TAG}"sprabhune/HelloNode:master
-docker push sprabhune/HelloNode:master
+docker rmi -f sprabhune/Hellonode:master || echo "master branch moving on"
+docker tag "sprabhune/Hellonode:${BUILD_TAG}"sprabhune/Hellonode:master
+docker push sprabhune/Hellonode:master
